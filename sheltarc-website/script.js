17495fd2688f792038a1ea1b983667cbb893b700
unknown <chima.okwuokei@pau.edu.ng>
2024-03-14 06:31:40 +0100

    chatbot

--- sheltarc-website/script.js
+++ sheltarc-website/script.js
@@ -51,3 +51,47 @@ var swiper = new Swiper(".home-slider",{
         prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper = new Swiper(".reviews-slider",{
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+            
+        },
+        768:{
+            slidesPerView: 2,
+            
+        },
+        991:{
+            slidesPerView: 3,
+           
+        },
+    },
+});
+
+var swiper = new Swiper(".logo-slider",{
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints:{
+        450:{
+            slidesPerView: 2,
+            
+        },
+        640:{
+            slidesPerView: 3,
+            
+        },
+        768:{
+            slidesPerView: 4,
+            
+        },
+        991:{
+            slidesPerView: 5,
+           
+        },
+    },
+});
